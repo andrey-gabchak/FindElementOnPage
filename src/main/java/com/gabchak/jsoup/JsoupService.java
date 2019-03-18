bb842233f9493bc83c327437ab1946ff7452fe89
gabchak <andrey.gabchak@gmail.com>
2019-03-18 23:28:18 +0200

    Removed useless method

--- src/main/java/com/gabchak/jsoup/JsoupService.java
+++ src/main/java/com/gabchak/jsoup/JsoupService.java
@@ -4,19 +4,14 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Optional;
 
 
 public class JsoupService {
 
     private final static String CHARSET_NAME = "utf8";
-
-    private static Logger LOGGER = LoggerFactory.getLogger(JsoupService.class);
 
     public Document getDocument(File htmlFile) throws IOException {
             return Jsoup.parse(
@@ -24,21 +19,6 @@ public class JsoupService {
                     CHARSET_NAME,
                     htmlFile.getAbsolutePath());
 
-    }
-
-    public Optional<Elements> findElementsByCssQuery(File htmlFile, String cssQuery) {
-        try {
-            Document doc = Jsoup.parse(
-                    htmlFile,
-                    CHARSET_NAME,
-                    htmlFile.getAbsolutePath());
-
-            return Optional.of(doc.select(cssQuery));
-
-        } catch (IOException e) {
-            LOGGER.error("Error reading [{}] file", htmlFile.getAbsolutePath(), e);
-            return Optional.empty();
-        }
     }
 
     public Elements findSimilarElements(File htmlFile, Element originalElement) throws IOException {
