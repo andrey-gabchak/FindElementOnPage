590a58f7dd2c81e3cc0585e824259539198d97c1
gabchak <andrey.gabchak@gmail.com>
2019-03-18 23:29:02 +0200

    Don't use custom exception anymore

--- src/main/java/com/gabchak/exceptions/ElementNotFoundExeption.java
+++
@@ -1,9 +0,0 @@
-package com.gabchak.exceptions;
-
-public class ElementNotFoundExeption extends RuntimeException {
-
-    @Override
-    public String getMessage() {
-        return "Element not found";
-    }
-}
