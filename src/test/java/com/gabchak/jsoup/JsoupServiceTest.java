ab20aed3820c4cef25d5142a413889decba0e3b9
gabchak <andrey.gabchak@gmail.com>
2019-03-26 03:40:11 +0200

    added test

---
+++ src/test/java/com/gabchak/jsoup/JsoupServiceTest.java
@@ -0,0 +1,35 @@
+package com.gabchak.jsoup;
+
+import com.gabchak.Factory;
+import org.jsoup.select.Elements;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsoupServiceTest {
+
+    @Test
+    void getSimilarElements() {
+        String originalElementId = "make-everything-ok-button";
+        String originalFilePath = "/home/gabchak/Downloads/AgileEngine_pages/sample-0-origin.html";
+        String[] filePathsForSearch = new String[]{"/home/gabchak/Downloads/AgileEngine_pages/sample-1-evil-gemini.html",
+                "/home/gabchak/Downloads/AgileEngine_pages/sample-2-container-and-clone.html",
+                "/home/gabchak/Downloads/AgileEngine_pages/sample-3-the-escape.html",
+                "/home/gabchak/Downloads/AgileEngine_pages/sample-4-the-mash.html"};
+
+        Elements similarElements = Factory.getJsoupService().getSimilarElements(originalFilePath,
+                originalElementId, filePathsForSearch);
+
+        String expected0 = "html > body > div > div > div > div > div > div > a";
+        String expected1 = "html > body > div > div > div > div > div > div > div > a";
+        String expected2 = "html > body > div > div > div > div > div > div > a";
+        String expected3 = "html > body > div > div > div > div > div > div > a";
+
+        OutputService outputService = Factory.getOutputService();
+
+        assertEquals(expected0, outputService.getPathAsString(similarElements.get(0)));
+        assertEquals(expected1, outputService.getPathAsString(similarElements.get(1)));
+        assertEquals(expected2, outputService.getPathAsString(similarElements.get(2)));
+        assertEquals(expected3, outputService.getPathAsString(similarElements.get(3)));
+    }
+}
